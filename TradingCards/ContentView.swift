1b43c1c0bd84fd466bcf249f76482b4639a9fad4
Lillian Yang <lyang@lcs.on.ca>
2021-11-06 10:42:48 -0400

    Initial Commit

---
+++ TradingCards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TradingCards
+//
+//  Created by Lillian Yang on 2021-11-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Lillian Yang on 2021-11-06.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
